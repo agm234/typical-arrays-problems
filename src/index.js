38d9335fd32c4debef8a43800b13ece477ce7b2f
agm234 <79218134+agm234@users.noreply.github.com>
2021-03-08 20:02:59 +0300

    feat: add  min & max & avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == 'undefined' || array.length == 0) {
+    return 0;
+  }
+  let mine = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mine) {
+      mine = array[i];
+    }
+  }
+  return mine;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == 'undefined' || array.length == 0) {
+    return 0;
+  }
+  let maxe = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxe) {
+      maxe = array[i];
+    }
+  }
+  return maxe;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == 'undefined' || array.length == 0) {
+    return 0;
+  }
+  let summ = 0;
+  let avge = 0;
+  for (let i = 0; i < array.length; i++) {
+    summ = summ + array[i];
+  }
+  avge = summ / array.length;
+  return avge;
 }
+

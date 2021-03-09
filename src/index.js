78ab4b4f7f384c855dec54cc4e8e0d42e7e48e54
agm234 <79218134+agm234@users.noreply.github.com>
2021-03-09 16:49:02 +0300

    fix: change min & max & avg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min(array) {
-  if (array == 'undefined' || array.length == 0) {
+  if (typeof array === 'undefined' || array.length == 0) {
     return 0;
   }
   let mine = array[0];
@@ -13,7 +13,7 @@ exports.min = function min(array) {
 }
 
 exports.max = function max(array) {
-  if (array == 'undefined' || array.length == 0) {
+  if (typeof array === 'undefined' || array.length == 0) {
     return 0;
   }
   let maxe = array[0];
@@ -26,7 +26,7 @@ exports.max = function max(array) {
 }
 
 exports.avg = function avg(array) {
-  if (array == 'undefined' || array.length == 0) {
+  if (typeof array === 'undefined' || array.length == 0) {
     return 0;
   }
   let summ = 0;
